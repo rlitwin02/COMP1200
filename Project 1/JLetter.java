4d34b532ee3cac2f185374e6b89c86bfc79ec235
rlitwin02 <54961399+rlitwin02@users.noreply.github.com>
2022-03-30 15:13:34 -0500

    Add files via upload

---
+++ Project 1/JLetter.java
@@ -0,0 +1,25 @@
+  /**
+   *Makes the letter J out of Java.
+   *author Ryan Litwin
+   *version 8/28/2019
+   */
+public class JLetter
+{
+ /**
+  *Where all the strings get used and output.
+  *@param args Command line arguments (not used).
+  */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
